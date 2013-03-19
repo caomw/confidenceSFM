b232360630d693c7f9b0c4f81f0ac613e9de8dd9
Lisandro <lkaunitz@gmail.com>
2013-03-19 19:59:16 -0300

    Modified srcPres/SFM_minimum.m and srcPres/getSFMDotPos.m. Now I am using 'InitializeScreen' and I am tracking the responses on each frame. It collects the data correctly
    new file:   notes.txt

--- confidenceSFM/srcPres/getSFMDotPos.m
+++ confidenceSFM/srcPres/getSFMDotPos.m
@@ -1,4 +1,5 @@
- function T = getSFMDotPos(SFM,disparity,direction,window)
+ function dot_textures = getSFMDotPos(SFM, disparity, direction, window)
+
 % nargin = 0;
 % compute nFrames for SFM
 if nargin < 2
@@ -13,18 +14,20 @@ dbstop if error
 isAnaglyph = 1;
 %initialize the dot positions
 nDots = SFM.nDots;
-dots.xflat(1:nDots) = [rand(nDots,1)-.5]*2;
-dots.yflat(1:nDots) = [rand(nDots,1)-.5]*2;
+dots.xflat(1:nDots) = (rand(nDots,1)-.5)*2;
+dots.yflat(1:nDots) = (rand(nDots,1)-.5)*2;
 
 nPix = SFM.dotSize^2;
-[dotX,dotY]= meshgrid([1:SFM.dotSize],[1:SFM.dotSize]);
+[dotX,dotY]= meshgrid(1:SFM.dotSize, 1:SFM.dotSize);
 dotX = dotX(:)';
 dotY = dotY(:)';
 
 blankImg = zeros(SFM.imgSize);
       
-dotBuffer = SFM.dotSize/2 / SFM.pixPerDeg ;
+dotBuffer = SFM.dotSize / 2 / SFM.pixPerDeg ;
 radius = SFM.radius-dotBuffer ;
+
+dot_textures = zeros(1,SFM.nframe);
 
 clear T
 for iFrame = 1:SFM.nframe
@@ -77,7 +80,7 @@ for iFrame = 1:SFM.nframe
         yy1 = repmat(round((dots.y(goodDots)'+SFM.radius)*SFM.pixPerDeg),1,nPix) + repmat(dotY,length(goodDots),1);
         yy2 = repmat(round((dots.y(goodDotsD)'+SFM.radius)*SFM.pixPerDeg),1,nPix) + repmat(dotY,length(goodDotsD),1);
 
-                img1((xx1(:))*SFM.imgSize+yy1(:)) = 1 ;
+        img1((xx1(:))*SFM.imgSize+yy1(:)) = 1 ;
         img2((xx2(:))*SFM.imgSize+yy2(:)) = 1 ;
         
         if isAnaglyph
@@ -90,16 +93,20 @@ for iFrame = 1:SFM.nframe
         dots.xflat = dots.xflat+ 2*SFM.degPerSec/360/SFM.frameRate * direction;
     end
     
-    if nargin < 4
-        % demo 
-        figure(1),hold off, 
-        cla,
-        drawnow,
-        imagesc(img3/255)
-        drawnow,
-        pause(0.1)
-    else
-        T(iFrame) = screen(window,'openoffscreenwindow',[],SFM.imgRect);
-        SCREEN(T(iFrame),'PutImage',img3)
-    end
+%     if nargin < 4
+%         % demo 
+%         figure(1),hold off, 
+%         cla,
+%         drawnow,
+%         imagesc(img3/255)
+%         drawnow,
+%         pause(0.1)
+%     else
+%         T(iFrame) = screen(window,'openoffscreenwindow',[],SFM.imgRect); %#ok
+%         SCREEN(T(iFrame),'PutImage',img3)
+%     end
+
+    dot_textures(iFrame) = Screen('MakeTexture', window, img3);
 end
+
+
