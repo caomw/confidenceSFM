47168124484e11cd620b41bc827056492b6aa128
Lisandro <lkaunitz@gmail.com>
2013-04-23 02:31:25 +1000

    Cleaned some other files that were not needed.
    deleted:    SND.m
    deleted:    WaitSecs.dll
    deleted:    drawDot.m
    deleted:    getDotPos.m
    deleted:    presentApparentMotion.asv
    deleted:    presentStructureFromMotion.asv
    deleted:    presentStructureFromMotion.m
    deleted:    runConf*

--- srcPres/drawDot.m
+++
@@ -1,5 +0,0 @@
-function drawDot(AM,pos,window)
-screen(window,'fillrect',AM.color,...
-    [AM.fixationXY AM.fixationXY] + [pos pos] + AM.size/2*[-1 -1 1 1])
-
-% disp( [pos pos] + AM.size/2*[-1 -1 1 1] ) 
