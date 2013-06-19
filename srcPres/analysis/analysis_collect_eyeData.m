ccc1c77ef88f2ac3def79d5c79d8c3a90df07744
lisandro <lisandro@lisandro-OptiPlex-9010.(none)>
2013-06-19 21:49:55 +1000

    Worked on analysis_eyeData to plot the first results. Added two graphs to this script
    new file:   notes_meetings/analysisEyeData.PDF
    new file:   presentations/meeting_24062013.odp
    new file:   srcPres/analysis/analysis_collect_eyeData.m

---
+++ srcPres/analysis/analysis_collect_eyeData.m
@@ -0,0 +1,201 @@
+function analysis_collect_eyeData
+
+% Collect all behavioral data and eye data into 'Run' structure for all
+% subjects
+
+% Loop through subjects
+data_dir = '../../dataRaw/';
+
+% Here we define all subjects that we are going to analyze
+subjects = {'MO' 'NS' 'TF'};
+
+
+% Loop through subjects
+for su = 1 : length(subjects)
+    
+    % Load each subject once at a time,if you don't specify any return
+    % variable to 'load' you'll just get 'Exp' on the workspace
+    load ( [data_dir subjects{su} ]);
+    
+    % Load eye event times
+    % OPEN FILE
+    fid_evs = fopen([data_dir subjects{su} '_Events.txt']);
+        
+    % Collect data from each trial
+    for rn = 1: length(Exp.Run)
+        for bk = 1: length(Exp.Run(rn).block)
+                          
+                % BEHAVIOR
+                Run(rn).block(bk).id =  Exp.Run(rn).block(bk).id; %#ok 
+                Run(rn).block(bk).idx = Exp.rnd_idxs(rn); %#ok
+                Run(rn).block(bk).nTrials =  Exp.Run(rn).block(bk).nTrials; %#ok
+                Run(rn).block(bk).stimFrames =  Exp.Run(rn).block(bk).stimFrames; %#ok
+                Run(rn).block(bk).interStimInterval =  Exp.Run(rn).block(bk).interStimInterval; %#ok
+                Run(rn).block(bk).response = Exp.Run(rn).block(bk).respType; %#ok
+                Run(rn).block(bk).response_times = (1: Exp.Run(rn).block(bk).stimFrames) / 60; %#ok % In Secs  
+                % Preallocate memory for later analysis
+                Run(rn).block(bk).rawEyeData = []; %#ok
+                Run(rn).block(bk).blinks = []; %#ok
+                Run(rn).block(bk).fixations = []; %#ok
+                Run(rn).block(bk).saccades = []; %#ok
+        end
+    end
+    
+    %% Search for the eye data time events
+    
+    % Loop through the text file    
+    run_idx = 0;  
+    bl_idxs = 1;
+    fix_idxs = 1;
+    sacc_idxs = 1;
+    
+    while (1)
+        
+        % get line by line of text file
+        tline = fgetl(fid_evs);
+        
+        %Check whether we've reached the end of the file
+        if ~ischar(tline), sprintf('End of Trials'), break, end;
+        %Skip line if it's an empty one.
+        if isempty(tline), continue, end;
+        
+        % Find next corresponding start of each run (they are indexed)        
+        if ~isempty(strfind(tline, 'UserEvent')) && ~isempty(regexp(tline, 'START_RUN_','once'))
+            
+            run_idx = run_idx + 1;
+            % Check that the run is consistent with the behavioural data
+            C= textscan(tline, '%s %n %n %n %s %s %s');
+            if strcmpi(C{7}, ['START_RUN_' num2str(Exp.rnd_idxs(run_idx))])
+                disp('Behavioral and eye data match for this run')
+            else
+                disp('Behavioral and eye data DO NOT MATCH for this run')
+                keyboard
+            end               
+            bk_idx = 0; % set block index
+            % go to next line
+            continue; 
+            
+        end
+        
+        % Find start of block
+        if ~isempty(strfind(tline, 'UserEvent')) && ~isempty(regexp(tline, 'START_BK','once'))
+            
+            bk_idx = bk_idx + 1;
+            messg = sprintf('Subject: %s, Run: %d, block: %d', subjects{su}, Exp.rnd_idxs(run_idx), bk_idx);
+            disp(messg)
+            tr_idx = 0; % set trial index
+            % go to next line
+            continue;          
+            
+        end
+        
+        % Collect times of trial events
+        if ~isempty(strfind(tline, 'UserEvent')) && ~isempty(regexp(tline, 'START_TR','once'))
+            
+            tr_idx = tr_idx + 1; 
+            C= textscan(tline, '%s %n %n %n %s %s %s');
+            Run(run_idx).block(bk_idx).trial_evTimes(tr_idx, 1) = C{4} / 1000000; %#ok % IN SECS          
+            continue;
+            
+        elseif ~isempty(strfind(tline, 'UserEvent')) && ~isempty(regexp(tline, 'END_TR','once'))
+            
+            C= textscan(tline, '%s %n %n %n %s %s %s');
+            Run(run_idx).block(bk_idx).trial_evTimes(tr_idx, 2) = C{4} / 1000000; %#ok
+            continue;
+        end
+        
+        % As the events are at the end of the .txt we need to search for them
+        % here separately and add them to the structure
+        
+        if ~isempty(regexp(tline, 'Fixation L','once'))
+
+            % Table Header for Fixations:
+            % Event Type	Trial	Number	Start	End	Duration	Location X	Location Y	Dispersion X	Dispersion Y	Plane	Avg. Pupil Size X	Avg Pupil Size Y
+            C = textscan(tline, '%s %s %n %n %n %n %n %n %n %n %n %n %n %n');
+            EventType = 1;
+            times_ev = [C{5:7}] ./ 1000000;
+            % Event_Type   Start	 End	 Duration	Location X	Location Y
+            fixations(fix_idxs, 1:6) = [EventType times_ev C{8} C{9}]; %#ok
+            fix_idxs = fix_idxs + 1;
+            continue;
+        end
+    
+        if ~isempty(regexp(tline, 'Saccade L','once'))
+            
+            % Table Header for Saccades:
+            % Event_Type	Trial	Number	Start	End	Duration	Start Loc.X	Start Loc.Y	End Loc.X	End Loc.Y	Amplitude	Peak Speed	Peak Speed At	Average Speed	Peak Accel.	Peak Decel.	Average Accel.
+            C = textscan(tline, '%s %s %n %n %n %n %n %n %n %n %n %n %n %n %n %n %n %n');
+            EventType = 2;
+            times_ev = [C{5:7}] ./ 1000000;
+            % Event_Type	Start	End	 Duration	Start Loc.X	 Start Loc.Y	End Loc.X	End Loc.Y	Amplitude
+            saccades(sacc_idxs, 1:9)= [EventType times_ev C{8:12}]; %#ok
+            sacc_idxs = sacc_idxs + 1;
+            continue;
+        end
+        
+        if ~isempty(regexp(tline, 'Blink L','once'))
+            
+            % Table Header for Blinks:
+            % Event_Type	Trial	Number	Start	End	Duration
+            C = textscan(tline, '%s %s %n %n %n %n %n');
+            EventType = 3;
+            times_ev = [C{5} C{6} C{7}] ./ 1000000;
+            % Event_Type	Start	End 	Duration
+            blinks(bl_idxs, 1:4) = [ EventType times_ev]; %#ok
+            bl_idxs = bl_idxs + 1;
+            continue;
+        end
+    
+    
+    end
+    
+    % Save all data to disk
+    save([data_dir subjects{su} '_eye_beh'], 'Exp','Run', 'blinks', 'saccades', 'fixations')
+    
+    
+    %% Finally, add the continuous data to the structure (this data is on a
+    %% separate file
+    
+    % OPEN FILE
+    fid_samps = fopen([data_dir subjects{su} '_Samples.txt']);
+    samp_idx = 1;
+    samps = nan(1800000,9); % Preallocate to avoid time, then cut the unused parts
+    while (1)
+        
+        % get line by line of text file
+        tline = fgetl(fid_samps);
+        
+        %Check whether we've reached the end of the file
+        if ~ischar(tline), sprintf('End of Trials'), break, end;
+        %Skip line if it's an empty one.
+        if isempty(tline), continue, end;
+        
+        if ~isempty(regexp(tline, 'SMP','once'))
+            
+            % Table Header for Sample data:
+            % Time	Type	Trial	L Dia X [px]	L Dia Y [px]	R Dia X [px]	R Dia Y [px]	L POR X [px]	L POR Y [px]	R POR X [px]	R POR Y [px]	Aux1
+            C = textscan(tline, '%n %s %n %n %n %n %n %n %n %n %n');
+            % Time L_Dia_X	L_Dia_Y R_Dia_X  R_Dia_Y L_POR_X  L_POR_Y  R_POR_X R_POR_Y [all in pixels]
+            samps(samp_idx, 1:9) = [ (C{1} / 1000000)  C{4:11}];
+            samp_idx = samp_idx + 1;
+        end
+        
+        %Show progress
+        if mod(samp_idx, 100000) == 0
+            messg = sprintf('Processing sample %d', samp_idx);
+            disp(messg);
+        end
+    end
+    
+    idxs = ~isnan(samps(:,1));
+    samps = samps(idxs, :); %#ok
+    save([data_dir subjects{su} '_eye_beh'], 'samps', '-append');
+    
+    %%
+    fclose('all'); % clean fids
+    clear Exp Run blinks saccades fixations samps;
+    
+end
+
+
+
--- srcPres/analysis/analysis_eyeData.m
+++ srcPres/analysis/analysis_eyeData.m
@@ -1,95 +1,167 @@
-function Run = analysis_eyeData
+function analysis_eyeData
 
-% Collect all behavioral data and eye data into 'Run' structure for all
-% subjects
+% Analyze and plot behavioral and eye data
+
+
+%% Load data
 
 % Loop through subjects
 data_dir = '../../dataRaw/';
+figs_dir = '../../figures/';
 
 % Here we define all subjects that we are going to analyze
 subjects = {'MO' 'NS' 'TF'};
 
+putDataTogether = 0;
+plotRaxXY = 0;
 
+%% Put all data together
 % Loop through subjects
+if putDataTogether
+    
+    for su = 1 : length(subjects)
+        
+        % Load each subject once at a time,if you don't specify any return
+        % variable to 'loafigured' you'll just get 'Exp' on the workspace
+        load ( [data_dir subjects{su} '_eye_beh']);
+        
+        % Add all the info about eye position for each trial: raw data, saccades, fix and
+        % blinks into Run
+        
+        for rn = 1: length(Run)
+            for bk = 1: length(Run(rn).block)
+                aux = [];
+                for tr = 1figure : size(Run(rn).block(bk).trial_evTimes, 1)
+                    
+                    % raw signal
+                    tr_times = Run(rn).block(bk).trial_evTimes(tr, :);
+                    rawData = samps(samps(:,1) >= tr_times(1) & samps(:,1) <= tr_times(2), :); %#ok
+                    Run(rn).block(bk).rawEyeData{1, tr} = rawData; %#ok
+                    aux = cat(1, aux, rawData);
+                    % eye events
+                    Run(rn).block(bk).blinks = blinks(blinks(:,2) >= tr_times(1) & blinks(:,2) <= tr_times(2), :); %#ok
+                    Run(rn).block(bk).fixations = fixations(fixations(:,2) >= tr_times(1) & fixations(:,3) <= tr_times(2), :); %#ok
+                    Run(rn).block(bk).saccades = saccades(saccades(:,2) >= tr_times(1) & saccades(:,3) <= tr_times(2), :); %#ok
+                    
+                end
+                Run(rn).block(bk).rawEyeDataCat = aux; %#ok
+            end
+            
+        end
+        
+        % Save
+        save([data_dir subjects{su} '_eye_beh'], 'Run', '-append');
+    end
+end
+
+%% plot x & y raw data across runs / blocks
+if plotRaxXY
+    for su = 1 : length(subjects)
+        
+        load ( [data_dir subjects{su} '_eye_beh']);
+        
+        
+        for rn = 1: length(Run) %#ok
+            
+            z = figure(1);
+            set(gcf, 'Position', [0 0 1800 300], 'PaperPositionMode', 'auto', 'InvertHardcopy', 'off')
+            
+            for bk = 1: length(Run(rn).block)
+                
+                % Avoid the zeros (blinks, loss of signal) in the data
+                % idxs = find (Run(rn).block(bk).rawEyeDataCat(:,6) ~= 0 & Run(rn).block(bk).rawEyeDataCat(:,7) ~= 0);
+                % y = Run(rn).block(bk).rawEyeDataCat(idxs, 7);
+                
+                xy = Run(rn).block(bk).rawEyeDataCat(Run(rn).block(bk).rawEyeDataCat(:,6) ~= 0 & Run(rn).block(bk).rawEyeDataCat(:,7) ~= 0, 6:7);
+                
+                subplot(1,length(Run(rn).block), bk)
+                plot(xy(:,1), xy(:,2), 'r--')
+                set(gca,'xLim', [1 Exp.Cfg.width], 'yLim', [1 Exp.Cfg.height])
+                xlabel('X Pos [pixels]', 'FontSize', 16, 'FontWeight', 'bold')
+                ylabel('Y Pos [pixels]', 'FontSize', 16, 'FontWeight', 'bold')
+                tit = sprintf('Subject: %s, Run: %d, Blocks: %s', Exp.Gral.SubjectName, rn, Run(rn).block(bk).id );
+                title(tit)
+                
+            end
+            
+            figName = sprintf('%s_RawData_XY_Run_%d_Blocks_%s', Exp.Gral.SubjectName, rn, Run(rn).block(bk).id );
+            print(z, '-dtiff', '-loose', [figs_dir figName ])
+        end
+        
+    end
+end
+%% Single trial plots (good and bad one)
+%
+% tr = 2;
+% x = Run(rn).block(bk).rawEyeData{tr}(:,6);
+% y = Run(rn).block(bk).rawEyeData{tr}(:,7);
+% figure
+% plot(x, y, 'r--')
+% set(gca,'xLim', [1 Exp.Cfg.width], 'yLim', [1 Exp.Cfg.height])
+
+
+%% plot X data alone to show OKN patterns
+%
+% tms = (1:120) ./ 60;
+x = round(rand(1,120));
+y = interp(x, 8)
+% Y = resample(X,500,120)
+% xi = (1:1000) ./ 500;
+% yi = interp1(tms,y,xi);
+
 for su = 1 : length(subjects)
     
-    % Load each subject once at a time,if you don't specify any return
-    % variable to 'load' you'll just get 'Exp' on the workspace
-    load ( [data_dir subjects{su} ]);
+    load ( [data_dir subjects{su} '_eye_beh']);
+    
+    for rn = 1: length(Run) %#ok
         
-    % Load eye event times
-    % OPEN FILE
-    fid_samps = fopen([data_dir subjects{su} '_Samples.txt']);
-    fid_evs = fopen([data_dir subjects{su} '_Events.txt']);
-   
-    % Collect data from each trial
-    for rn = 1: length(Exp.Run)
-       for bk = 1: length(Exp.Run(rn).block)
-          for tr = 1 : length(Exp.Run(rn).block(bk))
-              
-              % Behavior
-              Run(rn).block(bk).id =  Exp.Run(rn).block(bk).id;
-              Run(rn).block(bk).idx = Exp.rnd_idxs(rn);
-              Run(rn).block(bk).nTrials =  Exp.Run(rn).block(bk).nTrials;
-              Run(rn).block(bk).stimFrames =  Exp.Run(rn).block(bk).stimFrames;
-              Run(rn).block(bk).interStimInterval =  Exp.Run(rn).block(bk).interStimInterval;
-              Run(rn).block(bk).response = Exp.Run(rn).block(bk).respType;
-              Run(rn).block(bk).response_times = (1: Exp.Run(rn).block(bk).stimFrames) / 60; % In Secs
-              
-              % Eye data
-              
-              % Find next corresponding start of run (they are indexed)
-              % collect trials
-              % find the next end of run and move to the next run
-              
-              
-          end
-       end
-    end
-    
-    
-    %% As the events are at the end of the .txt we need to search for them
-    %% here separately and add them to the structure
-    
-    
-    
-    
-    %% Finally, add the continuous data to the structure (this data is on a
-    %% separate file
-    
-    % OPEN FILE
-    fid_samps = fopen([data_dir subjects{su} '_Samples.txt']);
-
-    
-    
-    %%    
-    fclose('all'); % clean fids
-     
-end
-
-
-%% FUnction definitions
-
-function [line tline] = search_text(filename,texts,maxlines)
-%search for texts in the first maxlines and gives both the line number and the
-%text
-line = [];
-tline = [];
-fid = fopen(filename);
-icount = 0;
-while (isempty(ferror(fid)) && icount<maxlines)
-    icount = icount+1;
-    tline  =  fgetl(fid);    %read one line
-    for i = 1:length(texts)  %for all searched texts
-        if ~isempty(strfind(tline,texts{i}))
-            line = icount;
-            fclose(fid);
-            return
+        z = figure(1);
+        set(gcf, 'Position', [0 0 1800 300], 'PaperPositionMode', 'auto', 'InvertHardcopy', 'off')
+        time_bk = [];
+        allBks_xy = [];
+        for bk = 1: length(Run(rn).block)
+            
+            % Avoid the zeros (blinks, loss of signal) in the data
+            xy = Run(rn).block(bk).rawEyeDataCat(Run(rn).block(bk).rawEyeDataCat(:,6) ~= 0 & Run(rn).block(bk).rawEyeDataCat(:,7) ~= 0, 6:7);
+            
+            if bk == 1
+                time_bk =  ( 1 : size(xy,1) ) / 500;
+            else
+                
+                next_times = ( size(time_bk, 2) + 1 : size(time_bk, 2)  + size(xy,1) ) ./ 500;
+                time_bk = cat(2, time_bk, next_times);
+            end
+            bk_end(bk) = time_bk(end); %#ok
+            
+            allBks_xy = cat(1, allBks_xy, xy);
+            
         end
+        
+        plot(time_bk, allBks_xy(:,1), 'r-')
+        set(gca,'yLim', [1 Exp.Cfg.height])
+        xlabel('Time [secs]', 'FontSize', 22, 'FontWeight', 'bold')
+        ylabel('X Pos [pixels]', 'FontSize', 22, 'FontWeight', 'bold')
+        tit = sprintf('Subject: %s, Run: %d, Blocks: %s', Exp.Gral.SubjectName, rn, Run(rn).block(bk).id );
+        title(tit, 'FontSize', 28, 'FontWeight', 'bold')        
+        
+        figName = sprintf('%s_XData_Alone_Run_%d_Blocks_%s', Exp.Gral.SubjectName, rn, Run(rn).block(bk).id );
+        print(z, '-dtiff', '-loose', [figs_dir figName ])
+        
     end
+    
 end
-fclose(fid);
-if isempty(line)
-    tline = [];
-end
+
+close all
+
+
+
+
+
+
+
+
+
+
+
+
 
